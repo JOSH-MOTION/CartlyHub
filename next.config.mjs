79641baaf15b3353b12cd7fb52f28355dd4d3855
JOSH-MOTION <joshuadoe168@gmail.com>
2026-05-01 11:45:28 +0000

    done

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'res.cloudinary.com', 'firebasestorage.googleapis.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: 'https://studio-572308010-90c24.firebaseapp.com/__/auth/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
