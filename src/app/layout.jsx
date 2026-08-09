cebe2f0bab0aa3eacae88a865b59a3881904cf35
JOSH-MOTION <joshuadoe168@gmail.com>
2026-08-09 10:26:52 +0000

    done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -161,6 +161,20 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-WGBSHGCE6Z');
           `}
         </Script>
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xzmvs309lb");
+            `,
+          }}
+        />
+        
         <link rel="preconnect" href="https://kit.fontawesome.com" />
         <link rel="preconnect" href="https://ka-f.fontawesome.com" />
 
